420749d106064cc5feffd584bc050d9d7cdfdd77
Esmeralda Molina Estrada <121475904+Melle23@users.noreply.github.com>
2025-02-21 09:38:39 -0700

    Cambios en la parte 2

--- MyDigimind/app/src/main/java/molina/esmeralda/mydigimind/ui/dashboard/DashboardFragment.kt
+++ MyDigimind/app/src/main/java/molina/esmeralda/mydigimind/ui/dashboard/DashboardFragment.kt
@@ -1,31 +1,93 @@
 package molina.esmeralda.mydigimind.ui.dashboard
 
+import android.annotation.SuppressLint
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import molina.esmeralda.mydigimind.R
+import molina.esmeralda.mydigimind.Recordatorio
 import molina.esmeralda.mydigimind.databinding.FragmentDashboardBinding
+import molina.esmeralda.mydigimind.ui.home.HomeFragment
 import molina.esmeralda.mydigimind.ui.home.HomeViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            ViewModelProvider(owner = this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container,false)
 
-        })
+        val btn_time: Button = root.findViewById(R.id.btn_time)
+
+        btn_time.setOnClickListener{
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener{ timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+
+                btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE), true).show()
+
+        }
+
+        val btn_save = root.findViewById(R.id.btn_save) as Button
+        val et_titulo = root.findViewById(R.id.titulo) as EditText
+        val checkMonday = root.findViewById(R.id.checkboxMonday) as CheckBox
+        val checkTuesday = root.findViewById(R.id.checkboxTuesday) as CheckBox
+        val checkWednesday = root.findViewById(R.id.checkboxWednesday) as CheckBox
+        val checkThursday = root.findViewById(R.id.checkboxThursday) as CheckBox
+        val checkFriday = root.findViewById(R.id.checkboxFriday) as CheckBox
+        val checkSaturday = root.findViewById(R.id.checkboxSaturday) as CheckBox
+        val checkSunday = root.findViewById(R.id.checkboxSunday) as CheckBox
+
+        btn_save.setOnClickListener{
+
+            var titulo = et_titulo.text.toString()
+            var tiempo = btn_time.text.toString()
+            var dias = ArrayList<String>()
+
+            if(checkMonday.isChecked)
+                dias.add("Monday")
+            if(checkTuesday.isChecked)
+                dias.add("Tuesday")
+            if(checkWednesday.isChecked)
+                dias.add("Wednesday")
+            if(checkThursday.isChecked)
+                dias.add("Thursday")
+            if(checkFriday.isChecked)
+                dias.add("Friday")
+            if(checkSaturday.isChecked)
+                dias.add("Saturday")
+            if(checkSunday.isChecked)
+                dias.add("Sunday")
+
+            var tarea = Recordatorio(titulo, dias.toString() ,tiempo)
+
+            HomeFragment.listaRecordatorios.add(tarea)
+            Toast.makeText(root.context, "Nueva tarea añadida", Toast.LENGTH_SHORT).show()
+        }
+
         return root
     }
 }
--- MyDigimind/app/src/main/java/molina/esmeralda/mydigimind/ui/home/HomeFragment.kt
+++ MyDigimind/app/src/main/java/molina/esmeralda/mydigimind/ui/home/HomeFragment.kt
@@ -1,29 +1,99 @@
 package molina.esmeralda.mydigimind.ui.home
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import molina.esmeralda.mydigimind.R
+import molina.esmeralda.mydigimind.Recordatorio
 import molina.esmeralda.mydigimind.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    private lateinit var homeViewModel: HomeViewModel
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+    private var adapter: GridViewAdapter? = null
 
-        })
+
+
+    companion object{
+        val listaRecordatorios = ArrayList<Recordatorio>()
+        var first = true
+    }
+
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val gridView: GridView = root.findViewById(R.id.gridViewRecordatorios)
+
+        if (first){
+            filltareas()
+            first = false
+        }
+
+        adapter = GridViewAdapter(requireContext(), listaRecordatorios)
+        gridView.adapter = adapter
+
         return root
+    }
+
+    fun filltareas(){
+        listaRecordatorios.add(Recordatorio("Practice 1", arrayListOf("Tuesday").toString(), "17:30"))
+        listaRecordatorios.add(Recordatorio("Practice 2",
+            arrayListOf("Monday", "Sunday").toString(), "17:40"))
+        listaRecordatorios.add(Recordatorio("Practice 3", arrayListOf("Wednesday").toString(), "14:00"))
+        listaRecordatorios.add(Recordatorio("Practice 4", arrayListOf("Saturday").toString(), "11:00"))
+        listaRecordatorios.add(Recordatorio("Practice 5", arrayListOf("Friday").toString(), "13:00"))
+        listaRecordatorios.add(Recordatorio("Practice 6", arrayListOf("Thursday").toString(), "10:40"))
+        listaRecordatorios.add(Recordatorio("Practice 7", arrayListOf("Monday").toString(), "12:00"))
+
+    }
+
+    private class GridViewAdapter() : BaseAdapter() {
+        var listaTareas = ArrayList<Recordatorio>()
+        var context: Context? = null
+
+        constructor(contexto: Context, tareas: ArrayList<Recordatorio>) : this() {
+            this.context = contexto
+            this.listaTareas = tareas
+        }
+
+
+        override fun getCount(): Int {
+            return listaTareas.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return listaTareas[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            var tarea = listaTareas[p0]
+            var inflador = LayoutInflater.from(this.context)
+            var vista = inflador.inflate(R.layout.recordatorio, null)
+
+            val nombre: TextView = vista.findViewById(R.id.tvNombreRecordatorio)
+            val tiempo: TextView = vista.findViewById(R.id.tvTiempoRecordatorio)
+            val dias: TextView = vista.findViewById(R.id.tvDiasRecordatorio)
+
+            nombre.setText(tarea.nombre)
+            tiempo.setText(tarea.tiempo)
+            dias.setText(tarea.dias.toString())
+
+            return vista
+        }
     }
 }
